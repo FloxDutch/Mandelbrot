57e7131fa2664d31809ff96b216edab9b7c242cd
Hubot <flgeerts@xs4all.nl>
2017-12-04 12:02:11 +0100

    Laatste versie so far

--- Mandelbrot/Mandelbrot/Form1.Designer.cs
+++ Mandelbrot/Mandelbrot/Form1.Designer.cs
@@ -28,126 +28,157 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.button1 = new System.Windows.Forms.Button();
-      this.textBox1 = new System.Windows.Forms.TextBox();
-      this.label1 = new System.Windows.Forms.Label();
-      this.label2 = new System.Windows.Forms.Label();
-      this.label3 = new System.Windows.Forms.Label();
-      this.label4 = new System.Windows.Forms.Label();
-      this.textBox2 = new System.Windows.Forms.TextBox();
-      this.textBox3 = new System.Windows.Forms.TextBox();
-      this.textBox4 = new System.Windows.Forms.TextBox();
-      this.SuspendLayout();
-      // 
-      // button1
-      // 
-      this.button1.Location = new System.Drawing.Point(501, 43);
-      this.button1.Name = "button1";
-      this.button1.Size = new System.Drawing.Size(90, 37);
-      this.button1.TabIndex = 1;
-      this.button1.Text = "GO!";
-      this.button1.UseVisualStyleBackColor = true;
-      // 
-      // textBox1
-      // 
-      this.textBox1.Location = new System.Drawing.Point(160, 19);
-      this.textBox1.Name = "textBox1";
-      this.textBox1.Size = new System.Drawing.Size(85, 20);
-      this.textBox1.TabIndex = 2;
-      this.textBox1.Text = "0";
-      // 
-      // label1
-      // 
-      this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(41, 21);
-      this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(57, 13);
-      this.label1.TabIndex = 3;
-      this.label1.Text = "X - midden";
-      // 
-      // label2
-      // 
-      this.label2.AutoSize = true;
-      this.label2.Location = new System.Drawing.Point(47, 59);
-      this.label2.Name = "label2";
-      this.label2.Size = new System.Drawing.Size(51, 13);
-      this.label2.TabIndex = 4;
-      this.label2.Text = "Y midden";
-      // 
-      // label3
-      // 
-      this.label3.AutoSize = true;
-      this.label3.Location = new System.Drawing.Point(323, 20);
-      this.label3.Name = "label3";
-      this.label3.Size = new System.Drawing.Size(38, 13);
-      this.label3.TabIndex = 5;
-      this.label3.Text = "schaal";
-      // 
-      // label4
-      // 
-      this.label4.AutoSize = true;
-      this.label4.Location = new System.Drawing.Point(325, 55);
-      this.label4.Name = "label4";
-      this.label4.Size = new System.Drawing.Size(26, 13);
-      this.label4.TabIndex = 6;
-      this.label4.Text = "max";
-      // 
-      // textBox2
-      // 
-      this.textBox2.Location = new System.Drawing.Point(164, 56);
-      this.textBox2.Name = "textBox2";
-      this.textBox2.Size = new System.Drawing.Size(80, 20);
-      this.textBox2.TabIndex = 7;
-      this.textBox2.Text = "0";
-      // 
-      // textBox3
-      // 
-      this.textBox3.Location = new System.Drawing.Point(397, 20);
-      this.textBox3.Name = "textBox3";
-      this.textBox3.Size = new System.Drawing.Size(64, 20);
-      this.textBox3.TabIndex = 8;
-      this.textBox3.Text = "0.01";
-      // 
-      // textBox4
-      // 
-      this.textBox4.Location = new System.Drawing.Point(400, 64);
-      this.textBox4.Name = "textBox4";
-      this.textBox4.Size = new System.Drawing.Size(41, 20);
-      this.textBox4.TabIndex = 9;
-      this.textBox4.Text = "100";
-      // 
-      // Form1
-      // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(734, 711);
-      this.Controls.Add(this.textBox4);
-      this.Controls.Add(this.textBox3);
-      this.Controls.Add(this.textBox2);
-      this.Controls.Add(this.label4);
-      this.Controls.Add(this.label3);
-      this.Controls.Add(this.label2);
-      this.Controls.Add(this.label1);
-      this.Controls.Add(this.textBox1);
-      this.Controls.Add(this.button1);
-      this.Name = "Form1";
-      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-      this.Text = "Form1";
-      this.ResumeLayout(false);
-      this.PerformLayout();
+            this.button = new System.Windows.Forms.Button();
+            this.XmiddenTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.YmiddenTextBox = new System.Windows.Forms.TextBox();
+            this.schaalTextBox = new System.Windows.Forms.TextBox();
+            this.maxTextBox = new System.Windows.Forms.TextBox();
+            this.kleurComboBox = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(634, 37);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(90, 37);
+            this.button.TabIndex = 1;
+            this.button.Text = "GO!";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // XmiddenTextBox
+            // 
+            this.XmiddenTextBox.Location = new System.Drawing.Point(73, 16);
+            this.XmiddenTextBox.Name = "XmiddenTextBox";
+            this.XmiddenTextBox.Size = new System.Drawing.Size(85, 20);
+            this.XmiddenTextBox.TabIndex = 2;
+            this.XmiddenTextBox.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X - midden";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Y - midden";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(208, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "schaal";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(208, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "max";
+            // 
+            // YmiddenTextBox
+            // 
+            this.YmiddenTextBox.Location = new System.Drawing.Point(73, 54);
+            this.YmiddenTextBox.Name = "YmiddenTextBox";
+            this.YmiddenTextBox.Size = new System.Drawing.Size(84, 20);
+            this.YmiddenTextBox.TabIndex = 7;
+            this.YmiddenTextBox.Text = "0";
+            // 
+            // schaalTextBox
+            // 
+            this.schaalTextBox.Location = new System.Drawing.Point(250, 19);
+            this.schaalTextBox.Name = "schaalTextBox";
+            this.schaalTextBox.Size = new System.Drawing.Size(64, 20);
+            this.schaalTextBox.TabIndex = 8;
+            this.schaalTextBox.Text = "1,00";
+            // 
+            // maxTextBox
+            // 
+            this.maxTextBox.Location = new System.Drawing.Point(250, 57);
+            this.maxTextBox.Name = "maxTextBox";
+            this.maxTextBox.Size = new System.Drawing.Size(64, 20);
+            this.maxTextBox.TabIndex = 9;
+            this.maxTextBox.Text = "100";
+            // 
+            // kleurComboBox
+            // 
+            this.kleurComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.kleurComboBox.FormattingEnabled = true;
+            this.kleurComboBox.Items.AddRange(new object[] {
+            "zwart-wit",
+            "rood-blauw"});
+            this.kleurComboBox.Location = new System.Drawing.Point(390, 19);
+            this.kleurComboBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.kleurComboBox.Name = "kleurComboBox";
+            this.kleurComboBox.Size = new System.Drawing.Size(92, 21);
+            this.kleurComboBox.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(356, 21);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "kleur";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 711);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.kleurComboBox);
+            this.Controls.Add(this.maxTextBox);
+            this.Controls.Add(this.schaalTextBox);
+            this.Controls.Add(this.YmiddenTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.XmiddenTextBox);
+            this.Controls.Add(this.button);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
         #endregion
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.TextBox XmiddenTextBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-  }
+        private System.Windows.Forms.TextBox YmiddenTextBox;
+        private System.Windows.Forms.TextBox schaalTextBox;
+        private System.Windows.Forms.TextBox maxTextBox;
+        private System.Windows.Forms.ComboBox kleurComboBox;
+        private System.Windows.Forms.Label label5;
+    }
 }
 
--- Mandelbrot/Mandelbrot/Form1.cs
+++ Mandelbrot/Mandelbrot/Form1.cs
@@ -10,75 +10,129 @@ using System.Windows.Forms;
 
 namespace Mandelbrot
 {
-  public partial class Form1 : Form
-  {
-    public Form1()
+    public partial class Form1 : Form
     {
-      InitializeComponent();
-      CreateBitmapAtRuntime();
-    }
 
-    Point startLocation = new Point(100, 100);
-    private int maxWidth = 500;
-    private int maxHeight = 500;
-
-    PictureBox pictureBox1 = new PictureBox();
-    public void CreateBitmapAtRuntime()
-    {
-      pictureBox1.Size = new Size(maxWidth, maxHeight);
-      pictureBox1.Location = startLocation;
-      this.Controls.Add(pictureBox1);
-
-      Bitmap flag = DrawBitMap();
-      pictureBox1.Image = flag;
-    }
-
-    private Bitmap DrawBitMap()
-    {
-      var bitMap = new Bitmap(maxWidth, maxHeight);
-      Graphics flagGraphics = Graphics.FromImage(bitMap);
-      List<int> mandelbrotGetallen = new List<int>();
-      var whitePixels = 0;
-      var blackPixels = 0;
-
-      for(int bitMapX = -maxWidth / 2; bitMapX < maxWidth / 2; bitMapX++)
-      {
-        for(int bitMapY = -maxHeight / 2; bitMapY < maxHeight / 2; bitMapY++)
+        public Form1()
         {
-          double x = (double)bitMapX / (maxWidth / 2);
-          double y = (double)bitMapY / (maxHeight / 2);
-
-          double a = 0;
-          double b = 0;
-          int t = 0;
-
-          while(Math.Sqrt(a * a) + Math.Sqrt(b * b) < 2)
-          {
-            a = a * a - b * b + x;
-            b = 2 * a * b + y;
-            t++;
-            if(t >= 100)
-            {
-              break;
-            }
-          }
-          mandelbrotGetallen.Add(t);
-
-          var locX = maxWidth / 2 + bitMapX;
-          var locY = maxHeight / 2 - bitMapY;
-          if(t % 2 != 0)
-          {
-            flagGraphics.FillRectangle(Brushes.White, new Rectangle(new Point(locX, locY), new Size(1, 1)));
-            whitePixels++;
-          }
-          else
-          {
-            flagGraphics.FillRectangle(Brushes.Black, new Rectangle(new Point(locX, locY), new Size(1, 1)));
-            blackPixels++;
-          }
+            InitializeComponent();
+            CreateBitmapAtRuntime();
         }
-      }
-      return bitMap;
+
+        Point startLocation = new Point(100, 100);
+        private int maxWidth = 500;
+        private int maxHeight = 500;
+
+        //We maken een picturebox en een bitmap
+        PictureBox pictureBox1 = new PictureBox();
+        public void CreateBitmapAtRuntime()
+        {
+            pictureBox1.Size = new Size(maxWidth, maxHeight);
+            pictureBox1.Location = startLocation;
+            Bitmap flag = DrawBitMap();
+            pictureBox1.Image = flag;
+            this.Controls.Add(pictureBox1);
+        }
+
+        public Bitmap DrawBitMap()
+        {
+            var bitMap = new Bitmap(maxWidth, maxHeight);
+            Graphics flagGraphics = Graphics.FromImage(bitMap);
+            List<int> mandelbrotGetallen = new List<int>();
+            var whitePixels = 0;
+            var blackPixels = 0;
+
+            for (int bitMapX = -maxWidth / 2; bitMapX < maxWidth / 2; bitMapX++)
+            {
+                for (int bitMapY = -maxHeight / 2; bitMapY < maxHeight / 2; bitMapY++)
+                {
+                    //De gebruiker kan zelf een nieuw x en y coördinaat kiezen
+                    double xMidden = 0;
+                    double yMidden = 0;
+                    xMidden = 100 * Double.Parse(XmiddenTextBox.Text);
+                    yMidden = 100 * Double.Parse(YmiddenTextBox.Text);
+
+                    //De gebruiker kan de schaal aanpassen
+                    double schaal = 1;
+                    schaal = Double.Parse(schaalTextBox.Text);
+
+                    double x = ((double)bitMapX / (maxWidth / 5) * schaal) + (xMidden);
+                    double y = ((double)bitMapY / (maxHeight / 5) * schaal) - (yMidden);
+
+                    double a = 0;
+                    double b = 0;
+                    int t = 1;
+
+                    //De gebruiker kan verschillende kleurenschema's kiezen
+                    int selectedIndex = kleurComboBox.SelectedIndex;
+
+                    //De gebruiker kan het maximale aantal herhalingen kiezen
+                    int maxHerhalingen = 100;
+                    maxHerhalingen = Int32.Parse(maxTextBox.Text);
+                    
+                    //Berekening van het Mandelgetal
+                    while ((a * a) + (b * b) < 4)
+                    {
+                        var aa = a * a - b * b;
+                        var bb = 2 * a * b;
+
+                        a = aa + x;
+                        b = bb + y;
+
+
+                        t++;
+                        if (t >= maxHerhalingen)
+                        {
+                            break;
+                        }
+                    }
+                    mandelbrotGetallen.Add(t);
+
+                    var locX = (maxWidth / 2 + bitMapX);
+                    var locY = (maxHeight / 2 - bitMapY);
+                    
+                
+                    if (selectedIndex == 1)
+                    {
+                        if (t % 2 != 0)
+                        {
+                            flagGraphics.FillRectangle(Brushes.Red, new Rectangle(new Point(locX, locY), new Size(1, 1)));
+                            whitePixels++;
+                        }
+                        else
+                        {
+                            flagGraphics.FillRectangle(Brushes.Blue, new Rectangle(new Point(locX, locY), new Size(1, 1)));
+                            blackPixels++;
+                        }
+                    }
+                    else
+                    {
+                        if (t % 2 != 0)
+                        {
+                            flagGraphics.FillRectangle(Brushes.White, new Rectangle(new Point(locX, locY), new Size(1, 1)));
+                            whitePixels++;
+                        }
+                        else
+                        {
+                            flagGraphics.FillRectangle(Brushes.Black, new Rectangle(new Point(locX, locY), new Size(1, 1)));
+                            blackPixels++;
+                        }
+                    }               
+                }
+            }
+            return bitMap;
+        }
+
+        //Als op GO! wordt gedrukt wordt het figuur aangepast, anders wordt het figuur met standaardwaarden geladen
+        private void button_Click(object sender, EventArgs e)
+        {                     
+            Bitmap flag = DrawBitMap();
+            pictureBox1.Image = flag;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
-  }
 }
